b8b04babbf28661d68ae24b63fabe1299b8d7b94
thkiaAdiba <athkiaadiba@gmail.com>
2024-05-14 14:32:21 +0600

    added deployment related code

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        "http://localhost:5173",
+        "https://my-eleventh-project-c0e85.web.app",
+        "https://my-eleventh-project-c0e85.firebaseapp.com"
     ],
     credentials: true
 }));
@@ -54,11 +56,17 @@ const verifyToken = (req, res, next) => {
     })
 }
 
+const cookeOption = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const database = client.db("herittageHotel");
@@ -73,11 +81,7 @@ async function run() {
             console.log('user for token', user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE, { expiresIn: '1h' })
 
-            res.cookie('token', token, {
-                httpOnly: true,
-                secure: true,
-                sameSite: 'none'
-            })
+            res.cookie('token', token, cookeOption)
             .send({success: true})
                 
         })
@@ -87,7 +91,7 @@ async function run() {
             console.log('logging out', user)
 
             res
-                .clearCookie('token', { maxAge: 0 })
+                .clearCookie('token', { ...cookeOption, maxAge: 0 })
                 .send({ success: true })
         })
 
@@ -133,7 +137,7 @@ async function run() {
             if(req.user.email !== req.params.email){
                 return res.status(403).send({message: 'forbidden access'})
             }
-            
+
             const result = await bookedRoomCollection.find({ email: req.params.email }).toArray();
             console.log(result)
             res.send(result);
@@ -234,7 +238,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
